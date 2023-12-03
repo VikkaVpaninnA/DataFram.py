a7ad05cacc19f0f183e7e63e5befadc31b948c4f
vik <vikki>
2023-12-03 18:02:52 +0300

    DataFram

---
+++ kontr.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import random 
+lst = ['robot'] * 10  
+lst += ['human'] * 10  
+random.shuffle(lst)  
+data = pd.DataFrame({'whoAmI':lst})
+print(pd.DataFrame({'human': data['whoAmI'] == 'human','robot': data['whoAmI'] == 'robot'}).astype(int).head(10))
